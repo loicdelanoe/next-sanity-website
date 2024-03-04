64170ca92082ac0aa1334a7959ab870354b9aa15
Loïc Delanoë <95628210+loicdelanoe@users.noreply.github.com>
2024-03-04 18:05:29 +0100

    Replace config value with sanity env

--- sanity/config/client-config.ts
+++ sanity/config/client-config.ts
@@ -1,8 +1,8 @@
 const config = {
-    projectId: 'qyql4ize',
-    dataset: 'production',
-    apiVersion: '2024-03-04',
+    projectId: process.env.SANITY_API_PROJECT_ID,
+    dataset: process.env.SANITY_API_DATASET,
     useCDN: false,
+    apiVersion: '2024-03-04',
 }
 
 export default config;
